aa2940c36686efa63c3a1b79ba3f848da5931090
Holado <holado@gmail.com>
2021-02-24 16:20:01 +0000

    Tests updated

--- test/test_allkinds.py
+++ test/test_allkinds.py
@@ -1454,14 +1454,13 @@ def test_munu(verbose=False):
 def test_vera(verbose=False):
     # vera að + so.nh.
     s = "Ég er ekki að skilja þetta."
-    check_sentence(s, [(1, 6, "P_VeraAð")])
-    s = "Ég var að fara út með ruslið þegar ég fékk símtalið."
-    check_sentence(s, [(1, 11, "P_VeraAð")])
+    check_sentence(s, [(1, 5, "P_VeraAð")])
+    s = "Ég var að fara í sund þegar ég fékk símtalið."
+    check_sentence(s, [(1, 5, "P_VeraAð")])
     s = "Hún er að skrifa vel."
-    check_sentence(s, [(1, 6, "P_VeraAð")])
+    check_sentence(s, [(1, 4, "P_VeraAð")])
     s = "Það gekk mikið á þegar hún var ekki að sofa."
-    pass
-
+    check_sentence(s, [(6, 9, "P_VeraAð")])
 
 def test_nhm(verbose=False):
     # s = "Ég ætla fara í búð."
