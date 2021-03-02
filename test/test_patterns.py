71fe65ecb23631e24a32fdba22f128b4b8a57ec2
thorunna <thar@hi.is>
2021-03-02 14:16:00 +0000

    added testing for af4að

--- test/test_patterns.py
+++ test/test_patterns.py
@@ -77,6 +77,114 @@ def test_verb_af(rc):
     s = "Hver er að leita af skrifstofuhúsnæði?"
     check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
 
+    s = "Hann dáist endalaust af þeim."
+    check_sentence(rc, s, [(1, 3, "P_WRONG_PREP_AF")])
+
+    s = "Hann hefur lengi dást af þeim."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+
+    s = "Jón gerir grín af því."
+    check_sentence(rc, s, [(2, 3, "P_WRONG_PREP_AF")])
+
+    s = "Þetta er mesta vitleysa sem ég hef gert grín af."
+    check_sentence(rc, s, [(8, 9, "P_WRONG_PREP_AF")])
+
+    s = "Jón kann það ekki utan af."
+    check_sentence(rc, s, [(4, 5, "P_WRONG_PREP_AF")])
+
+    s = "Jón leggur hann ekki af velli."
+    check_sentence(rc, s, [(4, 5, "P_WRONG_PREP_AF")])
+
+    s = "Jón hefur ekki lagt hann af velli."
+    check_sentence(rc, s, [(5, 6, "P_WRONG_PREP_AF")])
+
+    s = "Jón leiðir líkur af því."
+    check_sentence(rc, s, [(1, 3, "P_WRONG_PREP_AF")])
+
+    s = "Jón leiðir ekki líkur af því."
+    check_sentence(rc, s, [(1, 4, "P_WRONG_PREP_AF")])
+
+    s = "Jón leiðir líkum af því."
+    check_sentence(rc, s, [(1, 3, "P_WRONG_PREP_AF")])
+
+    s = "Jón leiðir rök af því."
+    check_sentence(rc, s, [(1, 3, "P_WRONG_PREP_AF")])
+
+    s = "Jón leitar af því."
+    check_sentence(rc, s, [(1, 2, "P_WRONG_PREP_AF")])
+
+    s = "Tíminn markar upphaf af því."
+    check_sentence(rc, s, [(1, 3, "P_WRONG_PREP_AF")])
+
+    s = "Tíminn markar ekki upphaf af því."
+    check_sentence(rc, s, [(1, 4, "P_WRONG_PREP_AF")])
+
+    s = "Það markar upphafið af því."
+    check_sentence(rc, s, [(1, 3, "P_WRONG_PREP_AF")])
+
+    s = "Það markar ekki upphafið af því."
+    check_sentence(rc, s, [(1, 4, "P_WRONG_PREP_AF")])
+
+    s = "Það hefur ekki markað upphafið af því."
+    check_sentence(rc, s, [(3, 5, "P_WRONG_PREP_AF")])
+
+    s = "Jón spyr af því."
+    check_sentence(rc, s, [(1, 2, "P_WRONG_PREP_AF")])
+
+    s = "Það sem Jón spurði ekki af var óljóst."
+    check_sentence(rc, s, [(3, 5, "P_WRONG_PREP_AF")])
+
+    s = "Jón stuðlar af því."
+    check_sentence(rc, s, [(1, 2, "P_WRONG_PREP_AF")])
+
+    s = "Honum varð af ósk sinni."
+    check_sentence(rc, s, [(2, 3, "P_WRONG_PREP_AF")])
+
+    s = "Honum hafði orðið af ósk sinni."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+
+    s = "Honum varð ekki af ósk sinni."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+
+    s = "Hann varð ekki uppvís af því."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+
+    s = "Jón varð vitni af þessu."
+    check_sentence(rc, s, [(2, 3, "P_WRONG_PREP_AF")])
+
+
+def test_noun_af(rc):
+    s = "Hann gerði þetta af beiðni hennar."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+    s = "Af beiðni hennar gerði hann þetta."
+    check_sentence(rc, s, [(0, 1, "P_WRONG_PREP_AF")])
+    s = "Það var gert af þeirri fyrirmynd."
+    check_sentence(rc, s, [(3, 5, "P_WRONG_PREP_AF")])
+    s = "Þau gera þetta af heiðnum sið."
+    check_sentence(rc, s, [(3, 5, "P_WRONG_PREP_AF")])
+    s = "Ég baka köku af því tilefni."
+    check_sentence(rc, s, [(3, 5, "P_WRONG_PREP_AF")])
+    s = "Þau veittu mér aðgang af kerfinu."
+    check_sentence(rc, s, [(3, 5, "P_WRONG_PREP_AF")])
+    s = "Aðgangur af kerfinu var veittur."
+    check_sentence(rc, s, [(0, 1, "P_WRONG_PREP_AF")])
+    s = "Drög af verkefninu eru tilbúin."
+    check_sentence(rc, s, [(0, 1, "P_WRONG_PREP_AF")])
+    s = "Þau kláruðu drög af verkefninu."
+    check_sentence(rc, s, [(2, 3, "P_WRONG_PREP_AF")])
+    s = "Grunnur af verkefninu er tilbúinn."
+    check_sentence(rc, s, [(0, 1, "P_WRONG_PREP_AF")])
+    s = "Hann lagði ekki grunninn af verkefninu."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+    s = "Þau gerðu leit af dótinu."
+    check_sentence(rc, s, [(2, 3, "P_WRONG_PREP_AF")])
+    s = "Leit af dótinu hefur ekki skilað árangri."
+    check_sentence(rc, s, [(0, 1, "P_WRONG_PREP_AF")])
+    s = "Þetta er lykillinn af velgengni."
+    check_sentence(rc, s, [(2, 3, "P_WRONG_PREP_AF")])
+    s = "Hann gaf mér uppskriftina af réttinum."
+    check_sentence(rc, s, [(3, 4, "P_WRONG_PREP_AF")])
+
 
 def test_placename_pp(rc):
     s = "Ég hef búið á Hafnarfirði alla mína tíð en flyt nú í Akureyri."
