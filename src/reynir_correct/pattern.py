6106e3a075c914a96298f8f9afcc0b7fb7e79482
thorunna <thar@hi.is>
2021-03-11 15:32:23 +0000

    improved handling for different error cat

--- src/reynir_correct/pattern.py
+++ src/reynir_correct/pattern.py
@@ -1132,7 +1132,15 @@ class PatternMatcher:
             p.append(
                 (
                     "leiða",  # Trigger lemma for this pattern
-                    "VP > { VP > { 'láta' } VP > { PP > { 'að' } VP > 'leiða' } }",
+                    "VP > { VP > { 'láta' } VP > { PP > { 'að' } VP > { 'leiða' } } }",
+                    cls.wrong_preposition_að_leiða,
+                    None,
+                )
+            )
+            p.append(
+                (
+                    "leiður",  # Trigger lemma for this pattern
+                    "VP > { VP > { 'láta' } VP > { PP > { 'að' } 'leiður' } }",
                     cls.wrong_preposition_að_leiða,
                     None,
                 )
